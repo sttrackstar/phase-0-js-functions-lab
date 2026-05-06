198e5b9cd0d18447ecbcef9cdf8d59138415bc62
sttrackstar <sonalt321@gmail.com>
2026-05-06 17:15:06 -0400

    Assignment is completed

--- index.js
+++ index.js
@@ -1,7 +1,23 @@
+function calculateTax(amount) {
+    return amount * 0.1; 
+}
 
+function convertToUpperCase(text) {
+    return text.toUpperCase();
+}
 
+function findMaximum(a, b) {
+    return Math.max(a, b);
+}
 
+function isPalindrome(word) {
+    return word === word.reverse();
+}
 
+function calculateDiscountedPrice(originalPrice, discountPercentage) {
+    const discountAmount = originalPrice * (discountPercentage / 100);
+    return originalPrice - discountAmount;
+}   
 
 // This is required for the test to function properly  
 module.exports = { calculateTax, convertToUpperCase, findMaximum, isPalindrome, calculateDiscountedPrice };
